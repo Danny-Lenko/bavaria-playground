c00bf8df9a34b3457056dc94c9e87943f8d6b6ca
DannyLenk <danylenko.14@gmail.com>
2021-12-05 16:44:04 +0200

    Update battleship.js

--- battleship.js
+++ battleship.js
@@ -15,7 +15,51 @@ let view = {
 };
 
 let model = {
+   boardSize: 7,
+   shipSize: 3,
+   shipsNumber: 3,
+   shipsSunk: 0,
+   ships: [
+      { locations: ['12', '13', '14'], hits: ['', '', ''] },
+      { locations: ['22', '32', '42'], hits: ['', '', ''] },
+      { locations: ['64', '65', '66'], hits: ['', '', ''] }
+   ],
+
+   fire: function(guess) {
+      for (let i = 0; i < this.shipsNumber; i++) {
+         let ship = this.ships[i];
+         let index = ship.locations.indexOf(guess);
+         if (index >= 0) {
+            ship.hits[index] = "hit";
+            view.displayHit(guess);
+            this.isSunk(ship);
+            return true;
+         }
+      }
+      view.displayMiss(guess);
+      return false;
+   },
+
+   isSunk: function(ship) {
+      for (let i = 0; i < this.shipSize; i++) {
+         if (ship.hits[i] !== "hit") {
+            return false;
+         }
+      }
+      this.shipsSunk++;
+      view.displayMessage("You sank my battleship!");
+      return true;
+   }
+
+};
+
+let controller = {
    
-}
+};
+
+model.fire('00');
+model.fire('64');
+model.fire('65');
+model.fire('66');
 
 
